5ee3688ff243b66f0795da7b43037fba612c4416
iqbalsnt <iqbalsnt@students.amikom.ac.id>
2021-04-13 12:32:11 +0700

    Responsi3360

---
+++ ResponsiPemrograman3360/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ResponsiPemrograman3360
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            karyawan kry1 = new karyawan("iqbal","20119090",3000000);
+            karyawan kry2 = new karyawan("santoso","20118080", 2000000);
+
+            if (kry1.Gajibulanan <= 0 || kry2.Gajibulanan <= 0)
+            {
+                kry1.Gajibulanan = 0;
+                kry2.Gajibulanan = 0;
+            }
+
+            Console.WriteLine("No   NIK/Nama\t\t Gaji");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("1.   {0} {1} \t {2}", kry1.Nik, kry1.Nama, kry1.Gajibulanan);
+            Console.WriteLine("2.   {0} {1} \t {2}", kry2.Nik, kry2.Nama, kry2.Gajibulanan);
+            Console.WriteLine();
+
+            Console.WriteLine("Alhamdulillah dapat kenaikan gaji 10%");
+            Console.WriteLine();
+            Console.WriteLine("No   NIK/Nama\t\t Gaji");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("1.   {0} {1} \t {2}", kry1.Nik, kry1.Nama, kry1.Naik);
+            Console.WriteLine("2.   {0} {1} \t {2}", kry2.Nik, kry2.Nama, kry2.Naik);
+        }
+    }
+}
---
+++ ResponsiPemrograman3360/karyawan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsiPemrograman3360
+{
+    class karyawan
+    {
+        public karyawan(string nama, string nik, double gajibulanan)
+        {
+            Nama = nama;
+            Nik = nik;
+            Gajibulanan = gajibulanan;
+            Naik = (gajibulanan * 0.1)+gajibulanan;
+        }
+
+
+        public string Nama { get; set; }
+        public string Nik { get; set; }
+        public double Gajibulanan { get; set; }
+        public double Naik { get; set; }
+    }
+}
